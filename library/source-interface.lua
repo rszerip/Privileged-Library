938b93823305681f369c293938c8e6acf7f30884
swe7z <166190774+swe7z@users.noreply.github.com>
2024-08-21 20:40:23 -0300

    update library/source-interface.lua

--- library/source-interface.lua
+++ library/source-interface.lua
@@ -13,7 +13,8 @@ local PrivilegedLibrary = {
 	Flags = {},
 	Theme = {
 		Default = {
-			TextFont = "Default", -- Default will use the various font faces used across Privileged
+			TextFont = "Default",
+			_TitleFont = "SourceSansBold",
 			TextColor = Color3.fromRGB(240, 240, 240),
 
 			Background = Color3.fromRGB(25, 25, 25),
